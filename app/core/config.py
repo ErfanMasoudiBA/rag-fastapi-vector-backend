5f6c3644a2208034a672d9f55b1cfaac6ff1e76e
ErfanMasoudiba <erfanmasoudiba@gmail.com>
2026-05-05 10:37:33 +0330

    setup project structure and configuration management

---
+++ app/core/config.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    # for adjusting environment variable
+    app_name: str = "FastAPI RAG Docs Assistant"
+    app_description: str = "A modular RAG API for document-based question answering."
+    app_version: str = "0.1.0"
+    debug: bool = True
+    # these are lowercases but in .env they read in uppercase format
+    # pydantic-settings do the mapping job
+    
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+    
+settings = Settings()
+# we make an instance that the entire of the project can import
+# whenever we want to read the settings, we could just:
+# from app.core.config import settings 
---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from app.api.routes.health import router as health_router
+from app.core.config import settings
+
+# import the main class and make an instance of fastapi
+app = FastAPI(
+    title=settings.app_name,
+    # title is the name of the api
+    description=settings.app_description,
+    version=settings.app_version,
+    debug=settings.debug,
+    # all of this shows in swagger and openapi schema
+)
+
+app.include_router(health_router)
